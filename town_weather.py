8af24c49245741ad5568a1154d23e60ee8571f9b
jamcoding123 <wrleemok@gmail.com>
2020-02-07 12:28:42 +0900

    upload

--- town_weather.py
+++ town_weather.py
@@ -1,6 +1,6 @@
 from urllib import request, parse
 from bs4 import BeautifulSoup
-
+import os
 town = input("현재 날씨가 궁금한 동네의 이름을 입력해주세요 : ")
 town_weather = parse.quote(town + "+날씨")
 
@@ -9,3 +9,4 @@ url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=' + 
 html = request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 print('현재 \'' + town + '\'은'+ soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text + '도 입니다.', sep='')
+os.system('pause')
